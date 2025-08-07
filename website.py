660c03513488ccc1a09971f4f5c73a4a39922dc8
damaleshaunak4 <88734832+damaleshaunak4@users.noreply.github.com>
2025-08-07 12:49:29 +0530

    Update website.py

--- website.py
+++ website.py
@@ -13,7 +13,7 @@ model = genai.GenerativeModel(model_name="gemini-2.5-flash")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.subheader("hi :wave:")
+    st.subheader("Hi :wave:")
     st.title("I'm AI")
 
 with col2:
@@ -22,7 +22,7 @@ with col2:
 st.title(' ')
 
 
-st.title("AI BOT")
+st.title("AI CHAT BOT")
 
 user_question = st.text_input("Ask me anything")
 
@@ -37,41 +37,41 @@ if st.button("Ask", use_container_width=True):
         st.warning("Please enter a question.")
 
 
-st.title("")
-st.title("my setup")
-st.image("images/setup.png")
+# st.title("")
+# st.title("my setup")
+# st.image("images/setup.png")
 
 
-st.write(' ')
-st.title('my skills ')
-st.slider('programming',0 ,100, 70)
-st.slider('designing', 0, 100, 85)
-st.slider('robotics', 0, 100, 70)
+# st.write(' ')
+# st.title('my skills ')
+# st.slider('programming',0 ,100, 70)
+# st.slider('designing', 0, 100, 85)
+# st.slider('robotics', 0, 100, 70)
 
 
-st.write(' ')
-st.title("My Jets")
+# st.write(' ')
+# st.title("My Jets")
 
-col1, col2, col3 =st.columns(3)
+# col1, col2, col3 =st.columns(3)
 
 
-with col1:
-    st.image("images/1.png")
-    st.image("images/6.jpg")
-    st.image("images/3.png")
+# with col1:
+#     st.image("images/1.png")
+#     st.image("images/6.jpg")
+#     st.image("images/3.png")
 
-with col2:
-        st.image("images/4.png")
-        st.image("images/8.avif")
-        st.image("images/2.png")
+# with col2:
+#         st.image("images/4.png")
+#         st.image("images/8.avif")
+#         st.image("images/2.png")
 
-with col3:
-    st.image("images/7.jpg")
-    st.image("images/9.jpg")
-    st.image("images/5.png")
+# with col3:
+#     st.image("images/7.jpg")
+#     st.image("images/9.jpg")
+#     st.image("images/5.png")
 
 
 st.subheader(' ')
 st.write('CONTACT')
 st.title('For any inquiries, please contact me at')
-st.subheader('contact@ai.com ')
+st.subheader('shaunak422101@gmail.com')
